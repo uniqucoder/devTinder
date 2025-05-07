8e6512e04233d19b9679af607114ab38302177b0
uniqucoder <chaitanyatupsamudre@gmail.com>
2025-05-07 23:15:00 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,22 @@
 const express = require("express");
 
 const app = express();
+
+
+app.get('/user',(req,res)=>{
+    res.send({"First Name": "Chaitanya", "Last Name": "Tupsamudre"});
+})
+
+app.post('/user',(req,res)=>(
+    res.send("Post Api Call")
+));
+
+app.delete('/user',(req,res)=>{
+    res.send("Delete user");
+})
+
+
+
 app.use((req,res)=>{
     res.send("Hello From server!");
 })

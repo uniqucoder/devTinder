f93f4b21a27d3bb9fd6c3a585e3d235d539efcba
uniqucoder <chaitanyatupsamudre@gmail.com>
2025-05-21 22:51:57 +0530

    added endpoints user, feed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const connectDB = require('./config/database');
 const app = express();
 const User = require('./models/user')
 const {adminAuth} = require('./middlewares/auth');
+
+// middleware to convert JSON to Javascript object
 app.use(express.json());
 
 app.post("/signup", async (req,res)=>{
@@ -23,6 +25,36 @@ app.post("/signup", async (req,res)=>{
     
 })
 
+// find user by email
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const user = await User.find({emailId: userEmail});  
+        if(user.length === 0){
+            res.status(404).send("User Not found");
+        } 
+        res.status(200).send(user); 
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+
+
+})
+
+// get all user API
+app.get("/feed",async (req,res)=>{
+     try{
+        const user = await User.find({});  
+        if(user.length === 0){
+            res.status(404).send("User Not found");
+        } 
+        res.status(200).send(user); 
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
+});
 connectDB()
 .then(()=>{
     console.log('DB connected');

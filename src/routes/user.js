70679b376cdbbb8a522e1da19e328bbd725ba949
uniqucoder <chaitanyatupsamudre@gmail.com>
2025-07-22 23:28:34 +0530

    feed api pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -76,12 +76,31 @@ userRouter.get('/user/connections', userAuth, async (req,res)=>{
 userRouter.get('/feed', userAuth, async (req,res)=>{
     try{
 
-        // User see the can all except
-        // 1. own card
-        // 2. Card of his connection
-        // 3. status is ignored 
-        // 4.  
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit; 
+        const page = parseInt(req.query.page) || 1;
 
+        const skip = (page - 1) * limit;
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or : [{fromUserId: loggedInUser._id},{toUserId : loggedInUser._id}],
+        }).select("fromUserId toUserId");
+
+        const hiddenUserFromFeed = new Set();
+        connectionRequests.forEach((req)=>{
+            hiddenUserFromFeed.add(req.fromUserId);
+            hiddenUserFromFeed.add(req.toUserId);
+        })
+
+        const users = await User.find({
+            $and : [
+                {_id : {$nin : Array.from(hiddenUserFromFeed)}},
+                {_id : {$ne : loggedInUser._id}}
+            ]
+        }).select(user_safe_data).skip(skip).limit(limit);
+
+        res.send(users);
 
     }
     catch(err){
